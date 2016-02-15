636c6e295f93b9c682546d586e786945cf6d6481
Rania Glass <rania.glass@gmail.com>
2016-02-14 20:25:14 -0500

    ArchiveCandidates

--- SSSCode/Modules/CreateFolders.py
+++ SSSCode/Modules/CreateFolders.py
@@ -4,14 +4,16 @@ import csv
 import shutil
 
 currentpath = os.path.realpath(__file__)
-directorypath = re.split(r"(.*)\\Modules\\CreateFolders.py", currentpath)
+directorypath = re.split(r"(.*)\\SSSCode\\Modules\\CreateFolders.py", currentpath)
 directorypath = directorypath[1]
 print directorypath
 #param masterList is the CSV file that contains the candidates names
 def createFolders():
     #write to the output directory
-    directory = directorypath + "\Staff Selection Output"
+    directory = directorypath + "\\SSSCodeTestingMaterial\StaffSelectionOutput"
+    print "dir", directory
     if not os.path.exists(directory + "\Male"):
+        print "no male directory exists"
         os.makedirs(directory + "\Male")
     if not os.path.exists(directory + "\Female"):
         os.makedirs(directory + "\Female")
